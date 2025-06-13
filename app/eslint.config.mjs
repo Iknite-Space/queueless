69a19364efd0bc22573d77cd77a7a81bdb034bfd
sedrickGerards <mubangsedrick@gmail.com>
2025-06-13 11:12:08 +0100

    configured the eslint config file for flat config

---
+++ app/eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import react from "eslint-plugin-react";
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    rules: {
+      // "no-unused-state": "error",
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "no-unused-vars": "warn",
+    },
+  },
+  react.configs.flat.recommended,
+];
