e23fbf53a50ebac6f02bf0ee68e0431e68acb21a
sedrickGerards <mubangsedrick@gmail.com>
2025-07-10 15:32:38 +0100

    creating component for service page

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,33 @@
 
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
 import './App.css';
 import { ComingSoonPage } from './components/ComingSoonPage';
-import React from 'react';
+import ServicePage from './components/ServicesPage';
+
 
 
 
 function App() {
   return (
-    <>
-    <ComingSoonPage />
+ <>
+    <Router>
+      <Routes>
+        {/* 👤 Default landing or placeholder */}
+        <Route path="/" element={<ComingSoonPage />} />
+
+        {/* 🎨 Design Reference Screen */}
+        <Route path="/services" element={<ServicePage />} />
+
+        {/* 🔒 Add admin/guest routes below as needed */}
+        {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
+        {/* <Route path="/organizations" element={<ServicesPage />} /> */}
+      </Routes>
+    </Router>
+
     </>
   );
 }
---
+++ app/src/components/ServicesPage.jsx
@@ -0,0 +1,20 @@
+import "../styles/ServicePage.css";
+
+function ServiceInfo(){
+  return <>
+  
+  </>
+}
+
+function ServicePage() {
+
+
+  return <div className="service-list-container">
+    <div className="list-of-services">
+      <h3>Service 01</h3>
+      <p> This is the description for the service 1 for this organization</p>
+    </div>
+  </div>;
+}
+
+export default ServicePage;
