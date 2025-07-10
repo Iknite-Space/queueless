744c5928cb390fe843837ced6982e8900348e3a8
Ndong Henry <ndonghenryndang@gmail.com>
2025-07-10 12:54:22 +0100

    added queries to create a service , get organisation working hours and service duration and insert generated slots into the slots table

--- api/db/query/organization.sql
+++ api/db/query/organization.sql
@@ -1,7 +1,27 @@
 
+-- name: CreateService :one
+INSERT INTO services (
+    organization_id, service_name, service_description, duration
+) VALUES (
+    $1, $2, $3, $4
+)
+RETURNING service_id;
+
 -- name: GetOrganizations :many
 SELECT * FROM organizations;
 
 -- name: GetServicesByOrganization :many
 SELECT * FROM services
 WHERE organization_id = $1;
+
+-- name: GetServiceWithOrgTimes :one
+SELECT s.service_id, s.duration, o.start_time, o.end_time
+FROM services s
+JOIN organizations o ON s.organization_id = o.organization_id
+WHERE s.service_id = $1;
+
+-- name: InsertSlotTemplate :exec
+INSERT INTO service_slot_templates (
+    service_id, start_time, end_time
+) VALUES ($1, $2, $3)
+ON CONFLICT (service_id, start_time) DO NOTHING;
