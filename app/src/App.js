ef481c2402a93dd5774fa641e5877e6fa1f87f09
sedrickGerards <mubangsedrick@gmail.com>
2025-07-11 16:38:13 +0100

    fetched data from backend and also did a basic styling for the services page

--- app/src/App.js
+++ app/src/App.js
@@ -9,18 +9,30 @@ import { ComingSoonPage } from './components/ComingSoonPage';
 import ServicesPage from './components/ServicesPage'; 
 import OrganizationCard from './components/OrganizationCard';
 
+
+import ServicesPage from './components/ServicesPage';
+
+
+
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<ComingSoonPage />} />
-                    <Route path="/organizations" element={<OrganizationCard />} />
-                    <Route path="/services" element={<ServicesPage />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+ <>
+    <Router>
+      <Routes>
+        {/*  Default landing or placeholder */}
+        <Route path="/" element={<ComingSoonPage />} />
+
+        {/*  service  Screen */}
+        <Route path="/services" element={<ServicesPage />} />
+
+                {/* organizations screen */}
+        <Route path="/organizations" element={<OrganizationCard />} />
+
+      </Routes>
+    </Router>
+
+    </>
+  );
 }
 
 export default App;
