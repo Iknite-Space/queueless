e3f50be36b1be8aa89f4ea927b046b602aa1ef0f
sedrickGerards <mubangsedrick@gmail.com>
2025-07-11 09:55:38 +0100

    solved conflict on reactapp-ci

--- app/src/App.js
+++ app/src/App.js
@@ -6,12 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css';
 import { ComingSoonPage } from './components/ComingSoonPage';
-<<<<<<< HEAD
 import ServicePage from './components/ServicesPage';
-
-=======
-import React from 'react';
->>>>>>> 0c206ee5006780bd088da03925c3af0073e0e652
 
 
 
--- app/src/components/ComingSoonPage.jsx
+++ app/src/components/ComingSoonPage.jsx
@@ -1,9 +1,7 @@
 import './ComingSoonPage.css'
 import React from 'react'
-<<<<<<< HEAD
 
-=======
->>>>>>> 0c206ee5006780bd088da03925c3af0073e0e652
+
 
 export function ComingSoonPage() {
   return (
