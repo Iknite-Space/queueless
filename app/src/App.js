7c5c91984609827d1499392c0b282b88c47515a8
sedrickGerards <mubangsedrick@gmail.com>
2025-07-12 16:19:21 +0100

    created the HTML structure for the input form that pops ones a customer clicks select slots

--- app/src/App.js
+++ app/src/App.js
@@ -7,23 +7,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import { ComingSoonPage } from './components/ComingSoonPage';
 import ServicesPage from './components/ServicesPage';
+import CustomerInput from './components/CustomerInput';
 
 
 
 function App() {
   return (
- <>
-    <Router>
-      <Routes>
-        {/*  Default landing or placeholder */}
-        <Route path="/" element={<ComingSoonPage />} />
+    <>
+      <Router>
+        <Routes>
+          {/*  Default landing or placeholder */}
+          <Route path="/" element={<ComingSoonPage />} />
 
-        {/*  service  Screen */}
-        <Route path="/services" element={<ServicesPage />} />
+          {/*  service  Screen */}
+          <Route path="/services" element={<ServicesPage />} />
 
-      </Routes>
-    </Router>
-
+          {/*  input  Screen */}
+          <Route path="/input" element={<CustomerInput />} />
+        </Routes>
+        
+      </Router>
     </>
   );
 }
