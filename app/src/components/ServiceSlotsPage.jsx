8cce2ee538cfb95a02262355ff69f4350f3be45d
Ndong Henry <ndonghenryndang@gmail.com>
2025-07-12 20:44:56 +0100

    Created a service slot page to display all available slots for each service showing the date

---
+++ app/src/components/ServiceSlotsPage.jsx
@@ -0,0 +1,18 @@
+
+
+import { WeekHeader } from './WeekHeader';
+
+export function ServiceSlotsPage() {
+
+  return (
+    <>
+      {/* <div className="title">
+        <h1>Select an appointment time</h1>
+        <h2>MTN Molyko</h2>
+        <h3>Sim card registrations</h3>
+      </div>
+       */}
+      <WeekHeader />
+    </>
+  );
+}
