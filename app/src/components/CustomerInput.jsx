ed361fb651094ee0ebd805e913fc1eefcc1ac60b
sedrickGerards <mubangsedrick@gmail.com>
2025-07-14 19:48:28 +0100

    completed design for the customer inputs that pops up when user clicks a slot

--- app/src/components/CustomerInput.jsx
+++ app/src/components/CustomerInput.jsx
@@ -1,20 +1,32 @@
 import React from "react";
+import '../styles/CustomerInput.css' 
 
-function CustomerInput(){
- return (
-  <div className="input-form-container"> 
-  <div>
-    <img src="/assets/images/input-form-img.png" alt="customer input form " width={400} />
-  </div>
-  <div className="customer-input">
-    <input></input>
-    <input type="email" name="" id="" />
-    <input type="number" name="" id="" />
-    <input type="submit" value="" />
-    <input type="reset" value="" />
-  </div>
-  </div>
- )
+function CustomerInput() {
+  return (
+    <div className="input-form-container">
+      <div className="input-image">
+        <img
+          src="/assets/images/input-form-img.png"
+          alt="customer input form "
+          width={400}
+        />
+      </div>
+      <div className="customer-details">
+
+        <div className="customer-input">
+
+        <input type="text" placeholder="Enter Username" id="" />
+        <input type="email" placeholder="Example@example.com" id="" />
+        <input type="tel" placeholder="Momo Number (+237)" id="" />
+        </div>
+
+        <div className="input-form-actions">
+          <button className="cancel-button">Cancel</button>
+          <button className="submit-button">Submit</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CustomerInput;
