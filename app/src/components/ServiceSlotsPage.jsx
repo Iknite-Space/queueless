e6d3166a6b5a8f0b9071f157ea4506cca3d7d80e
Ndong Henry <ndonghenryndang@gmail.com>
2025-07-15 19:20:08 +0100

    Updated the serviceslotspage to show the monthly calendar

--- app/src/components/ServiceSlotsPage.jsx
+++ app/src/components/ServiceSlotsPage.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 
 import { WeekHeader } from './WeekHeader';
+import { DatePicker } from './DatePicker';
+import './ServiceSlotsPage.css'
 
 export function ServiceSlotsPage() {
 
   return (
-    <>
+    <div className='slot-page-container'>
       {/* <div className="title">
         <h1>Select an appointment time</h1>
         <h2>MTN Molyko</h2>
         <h3>Sim card registrations</h3>
       </div>
        */}
+       <div className="calendar">
+        <DatePicker />
+       </div>
+       
       <WeekHeader />
-    </>
+    </div>
   );
 }
