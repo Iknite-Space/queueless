7426aea0ebed556b539623a41ee1b96b51e48ba5
Ndong Henry <ndonghenryndang@gmail.com>
2025-07-21 15:24:32 +0100

    adjusted the confirm slot modal to display on the service slot page appropriately.

--- app/src/components/Modal.jsx
+++ app/src/components/Modal.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import CustomerInput from "./CustomerInput";
+import './Modal.css'
 
+import PropTypes from 'prop-types';
 
-function Modal() {
+Modal.propTypes = {
+  handleCloseModal: PropTypes.func.isRequired, 
+  showModal: PropTypes.func.isRequired, 
+};
+
+function Modal({showModal, handleCloseModal}) {
   return (
     <>
-      <div className="modal-overlay input-background">
-        <div className="modal-content">
-          <CustomerInput />
+      <div 
+        className={`modal-overlay nput-background ${showModal ? 'modal-show' : ''}`}
+        onClick={handleCloseModal}   
+      >
+        <div className="modal-content"
+         onClick={(e) => e.stopPropagation()}
+        >
+          <CustomerInput handleCloseModal={handleCloseModal} />
         </div>
       </div>
     </>
