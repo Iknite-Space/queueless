df135a2509154ed5fac6484356297b95749c8452
sedrickGerards <mubangsedrick@gmail.com>
2025-08-01 23:42:33 +0100

    added hero image and created structure for landing page

--- app/src/pages/landingPage/ComingSoonPage.jsx
+++ app/src/pages/landingPage/ComingSoonPage.jsx
@@ -7,8 +7,6 @@ import { useNavigate } from "react-router";
 
 import Header from "../../components/header/Header";
 
-
-
 export function ComingSoonPage() {
   const navigate = useNavigate();
 
@@ -18,14 +16,29 @@ export function ComingSoonPage() {
 
   return (
     <>
-      <Header />
-      <header className="coming-soon-container">
-        <h1 className="project-title">Qless</h1>
-      </header>
+      <nav>
+        <Header />
+      </nav>
+      <main>
+        <div className="preview-text">
+          <p className="head-text">Book Appointment For Your Service Easily</p>
+          <p className="description">
+            Browse trusted organizations. Pick a service. Reserve your time slot
+            instantly. Register your organization
+          </p>
+          <div className="button-container">
+            
+            <button className="navigation-button">Register Organization</button>
+            <button className="navigation-button" onClick={handleClick}>
+              View Organisations
+            </button>
+          </div>
+        </div>
+        <div className="hero-image">
+          <img src="/assets/images/hero-image.svg" alt="hero caption" />
+        </div>
+      </main>
 
-      <button className="navigation-button" onClick={handleClick}>
-        View Organisations
-      </button>
       {/* <Footer /> */}
     </>
   );
