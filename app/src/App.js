7f98f5b95b4c45d11feec1b8b08e1b803ba3b8cb
sedrickGerards <mubangsedrick@gmail.com>
2025-08-15 17:08:09 +0100

    structured the pages with header and those without and also display the elements using <Outlet /> from  react router for nested route

--- app/src/App.js
+++ app/src/App.js
@@ -13,43 +13,49 @@ import { ContactUs } from "./pages/ContactUsPage/ContactUs";
 import { Authentication } from "./components/Authentication/Authentication";
 import OrganizationDashboard from "./pages/OrganizationHomePage/Dashboard";
 import { CreateServiceComponent } from "./components/CreateServiceComponent/CreateServiceComponent";
-import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
+import HeaderLayout from "./components/layouts/HeaderLayout";
+import PlainLayout from "./components/layouts/PlainLayout";
 
 function App() {
   return (
     <div className="app-layout">
       <Router>
-        <Header />
         <div className="page-content">
-        <Routes>
-          {/*  Default landing or placeholder */}
-          <Route path="/" element={<LandingPage />} />
+          <Routes>
+            {/* Routes that contain the main header */}
+            <Route element={<HeaderLayout />}>
+              {/*  Default landing or placeholder */}
+              <Route path="/" element={<LandingPage />} />
 
-          {/* organizations screen */}
-          <Route path="/organizations" element={<OrganizationCard />} />
+              {/* organizations screen */}
+              <Route path="/organizations" element={<OrganizationCard />} />
 
-          {/*  service  Screen */}
-          <Route path="/:orgId/services" element={<ServicesPage />} />
+              {/*  service  Screen */}
+              <Route path="/:orgId/services" element={<ServicesPage />} />
 
-          <Route
-            path="/service/:serviceId/slots"
-            element={<ServiceSlotsPage />}
-          />
+              <Route
+                path="/service/:serviceId/slots"
+                element={<ServiceSlotsPage />}
+              />
+            </Route>
 
-          <Route path="/contact" element={<ContactUs />} />
+            <Route element={<PlainLayout/>}>
+              <Route path="/contact" element={<ContactUs />} />
 
-          <Route path="/login" element={<Authentication />} />
-          <Route path="/organization//*" element={<OrganizationDashboard />} />
-          <Route path="/create-service" element={<CreateServiceComponent />} />
-
-        </Routes>
+              <Route path="/login" element={<Authentication />} />
+              <Route
+                path="/organization//*"
+                element={<OrganizationDashboard />}
+              />
+              <Route
+                path="/create-service"
+                element={<CreateServiceComponent />}
+              />
+            </Route>
+          </Routes>
         </div>
-        <Footer />
       </Router>
-
     </div>
-
   );
 }
 
