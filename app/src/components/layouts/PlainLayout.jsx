602d74d3058c540e3562bd3ec7dc11af64235b48
sedrickGerards <mubangsedrick@gmail.com>
2025-08-15 17:20:37 +0100

    removed footer from the individual files and setting them globally on rthe app.js file

--- app/src/components/layouts/PlainLayout.jsx
+++ app/src/components/layouts/PlainLayout.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import Footer from "../footer/Footer";
 import PropTypes from "prop-types"
 import { Outlet } from "react-router";
 
 
 const PlainLayout = () => (
-  <>
+
     <main><Outlet /></main>
-    <Footer />
-  </>
+  
 );
 
 PlainLayout.propTypes ={
