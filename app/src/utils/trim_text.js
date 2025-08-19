03320a35d2ec81f53556efa0787b60faaab048d0
Ndong Henry <ndonghenryndang@gmail.com>
2025-08-19 11:42:45 +0100

    created a utility function to trim the service description.

---
+++ app/src/utils/trim_text.js
@@ -0,0 +1,8 @@
+// helper to trim description to 30 words max
+  export const truncateDescription = (text, wordLimit) => {
+    const words = text.split(" ");
+    if (words.length > wordLimit) {
+      return words.slice(0, wordLimit).join(" ") + " ...";
+    }
+    return text;
+  };
