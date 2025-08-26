4484dd650f2daeb8118d3e5f15340a43b90354d8
sedrickGerards <mubangsedrick@gmail.com>
2025-08-26 18:37:42 +0100

    adjusted the landing page content to include the carousel component

--- app/src/pages/landingPage/LandingPage.jsx
+++ app/src/pages/landingPage/LandingPage.jsx
@@ -1,69 +1,69 @@
-// import React from "react";
-// import "./LandingPage.css";
-// import { useNavigate } from "react-router";
-// import Header from "../../components/header/Header";
-// import About from "../../components/aboutUs/About";
-
-
-// export function LandingPage() {
-//   const navigate = useNavigate();
-
-//   const handleClickBookSlot = () => {
-//     navigate("/organizations");
-//   };
-
-
-//   return (
-//     <div className="landing-page-body">
-//       <Header />
-//       <main>
-//         <img
-//           src="/assets/images/waiting.jpg"
-//           alt="main-background"
-//           className="main-bg-image"
-//         />
-//         <div className="main-container">
-//           <div className="preview-box">
-//             <p className="head-text">Book your spot in seconds.</p>
-//             <p className="head-text">Skip the queue</p>
-//             <div className="description">
-//               <p>
-//                 Find the services you want and reserve your a spot for a
-//                 hassle-free visit.
-//               </p>
-//             </div>
-
-//             <div className="button-container">
-//               <button
-//                 className="navigation-button"
-//                 onClick={handleClickBookSlot}
-//               >
-//                 View Organization
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </main>
-
-//       <About onClickBookService={handleClickBookSlot} />
-
-//     </div>
-//   );
-// }
-
 import React from "react";
 import "./LandingPage.css";
 import { useNavigate } from "react-router";
 import About from "../../components/aboutUs/About";
 import Carousel from "../../components/carousel/Carousel";
 
+
+
 export function LandingPage() {
   const navigate = useNavigate();
 
+  const handleClickBookSlot = () => {
+    navigate("/organizations");
+  };
+
+
   return (
     <div className="landing-page-body">
-      <Carousel navigate={navigate} />
-      <About onClickBookService={() => navigate("/organizations")} />
- </div>
+      <main>
+        <img
+          src="/assets/images/waiting.jpg"
+          alt="main-background"
+          className="main-bg-image"
+        />
+        <Carousel navigate={navigate} />
+        {/* <div className="main-container">
+          <div className="preview-box">
+            <p className="head-text">Book your spot in seconds.</p>
+            <p className="head-text">Skip the queue</p>
+            <div className="description">
+              <p>
+                Find the services you want and reserve your a spot for a
+                hassle-free visit.
+              </p>
+            </div>
+
+            <div className="button-container">
+              <button
+                className="navigation-button"
+                onClick={handleClickBookSlot}
+              >
+                View Organization
+              </button>
+            </div>
+          </div>
+        </div> */}
+      </main>
+
+      <About onClickBookService={handleClickBookSlot} />
+    </div>
   );
 }
+
+// import React from "react";
+// import "./LandingPage.css";
+// import { useNavigate } from "react-router";
+// import About from "../../components/aboutUs/About";
+// import Carousel from "../../components/carousel/Carousel";
+
+// export function LandingPage() {
+//   const navigate = useNavigate();
+
+//   return (
+//     <div className="landing-page-body">
+//       <Carousel navigate={navigate} />
+//       <About onClickBookService={() => navigate("/organizations")} />
+//  </div>
+//   );
+// }
