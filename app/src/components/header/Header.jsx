c2c609ec39f9a3e9452eec15f4f59f06c730e955
sedrickGerards <mubangsedrick@gmail.com>
2025-08-26 20:08:26 +0100

    fixed toggle section of the header to display on click icon and also render the bacground unactive when the side menu is open

--- app/src/components/header/Header.jsx
+++ app/src/components/header/Header.jsx
@@ -39,7 +39,7 @@
 
 // export default Header
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -47,36 +47,50 @@ import { GiHamburgerMenu } from "react-icons/gi";
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isMenuOpen]);
+
   return (
-    <header className="header">
-      <div className="logo">
-        <h1>qless.</h1>
-      </div>
+    <>
+      <header className="header">
+        <div className="logo">
+          <h1>qless.</h1>
+        </div>
 
-      <nav className={`nav-bar ${isMenuOpen ? "open" : ""}`}>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#services">Service</a>
-          </li>
-          <li>
-            <Link to="/contact" className="register-nav">
-              Register
-            </Link>
-          </li>
-        </ul>
-      </nav>
+        <nav className={`nav-bar ${isMenuOpen ? "open" : ""}`}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#services">Service</a>
+            </li>
+            <li>
+              <Link to="/contact" className="register-nav">
+                Register
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-      <GiHamburgerMenu
-        className="hamburger-icon"
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-      />
-    </header>
+        <GiHamburgerMenu
+          className="hamburger-icon"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
+      </header>
+
+      {isMenuOpen && (
+        <div className="overlay" onClick={() => setIsMenuOpen(false)} />
+      )}
+    </>
   );
 }
 
