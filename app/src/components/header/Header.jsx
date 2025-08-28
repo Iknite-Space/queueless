9f4261aa566b134134bd2966226614bf5ec35c14
sedrickGerards <mubangsedrick@gmail.com>
2025-08-28 13:55:09 +0100

    fixed registration on hover effect on tthe nav-bar

--- app/src/components/header/Header.jsx
+++ app/src/components/header/Header.jsx
@@ -1,43 +1,4 @@
-// import React from 'react'
-// import './Header.css'
-// // import SearchInput from '../searchInput/SearchInput';
-// import { Link } from "react-router-dom";
-// import { GiHamburgerMenu } from "react-icons/gi";
 
-// function Header() {
-
-//   return (
-//     <div className="header">
-//       <div className="logo">
-//         <h1>qless.</h1>
-//       </div>
-//       <div className="nav-bar">
-//         <ul>
-//           <nav>
-//             <Link to="/">Home</Link>
-//           </nav>
-//           <nav>
-//             <a href="#about">About</a>
-//           </nav>
-//           <nav>
-//             <a href="#services">Service</a>
-//           </nav>
-//           <nav>
-//             {/* preferable to use link so as not to re-load the pages but does the single page rendering */}
-//             <Link to="/contact" className="register-nav">
-//               Register
-//             </Link>
-//             {/* <a href="/contact">Services</a> */}
-//           </nav>
-//         <GiHamburgerMenu className='hamburger-icon'/>
-//         </ul>
-//       </div>
-//       <div className="search-bar-container">{/* <SearchInput /> */}</div>
-//     </div>
-//   );
-// };
-
-// export default Header
 
 import React, { useState, useEffect } from "react";
 import "./Header.css";
@@ -59,7 +20,7 @@ function Header() {
     <>
       <header className="header">
         <div className="logo">
-          <h1>qless.</h1>
+          <h1>QLess.</h1>
         </div>
 
         <nav className={`nav-bar ${isMenuOpen ? "open" : ""}`}>
